986d34430f71a68b269938b2d9d84949af14d427
Kai Wang <kaiuchihaa@gmail.com>
2022-02-14 01:15:21 -0300

    new works
    new works

--- HangmanGame.c
+++ HangmanGame.c
@@ -3,62 +3,57 @@
 #include <math.h>
 #include <string.h>
 
-
-
-int main(void){
-
+void opengame(){
     printf("\n-----------------------------------------------------------\n");
     printf("------------------WELCOME TO HANGMAN GAME------------------\n");
     printf("-----------------------------------------------------------\n");
+}
+void finalgame(){
+    printf("\n-----------------------------------------------------------\n");
+    printf("----------------------THE END GAME-------------------------\n");
+    printf("-----------------------------------------------------------\n");
+}
 
-    // palavras são arrays que deve ser colocados o qtd de caracteres dentro de []
-    // sprintf printa todo o array não somente palavra por palavra
-    char secretyword[20];
-    sprintf(secretyword, "watermelon");
-    printf("%s \n", secretyword);
+void youwrong(){
+    printf("     OH MY GODS YOU BE HANGED   \n");
+            printf("                                \n");
+            printf("    _______________             \n");  
+            printf("   /               \\           \n");
+            printf("  /                 \\          \n");
+            printf("//                   \\/\\      \n");
+            printf("\\|   XXXX     XXXX   | /       \n");
+            printf(" |   XXXX     XXXX   |/         \n");
+            printf(" |   XXX       XXX   |          \n");
+            printf(" |                   |          \n");
+            printf(" \\__      XXX      __/         \n");
+            printf("   |\\     XXX     /|           \n");
+            printf("   | |           | |            \n");
+            printf("   | I I I I I I I |            \n");
+            printf("   |  I I I I I I  |            \n");
+            printf("   \\_             _/           \n");
+            printf("     \\_         _/             \n");
+            printf("       \\_______/               \n");
+}
 
-    int acertou, enforcou, erros, hits;
-    acertou = 0;
-    enforcou = 0;
-    erros = 0;
-    hits = 0;
-    
-    
-    char hangmanstring[strlen(secretyword)];
-    for( int y = 0; y < strlen(secretyword); y ++){
-        hangmanstring[y] = '_';
-    }
+void youwin(){
+    printf(" CONGRATULATIONS YOU WIN \n");
+            printf("                         \n");
+            printf("       ___________       \n");
+            printf("      '._==_==_=_.'      \n");
+            printf("      .-\\:      /-.      \n");
+            printf("     | (|:.     |) |     \n");
+            printf("      '-|:.     |-'      \n");
+            printf("        \\::.    /        \n");
+            printf("         '::. .'         \n");
+            printf("           ) (           \n");
+            printf("         _.' '._         \n");
+            printf("        '-------'        \n");
+}
 
-    while(acertou == 0 || enforcou == 0){
+verificar(int *erros, int *hits, int manipulador, int *enforcou, int *acertou){
+    if((*erros) == 1){
 
-        char letter;
-        printf("enter your first guess for the letter in the secret word. \n");
-        printf("acetou: %d errou: %d \n", hits, erros);
-        scanf(" %c", &letter);
-
-        for(int c = 0; c <= strlen(secretyword); c++){
-            if(secretyword[c] == letter){
-                printf("you hit it!!! \n");
-                printf("------------- a psicao %d----------------------\n\n", c);
-                hangmanstring[c] = letter;
-                hits ++;
-
-            }
-            /*else{
-
-                printf("the secret letter is : $s \n", letter);
-
-                printf("you don't hit it please try again\n");
-                erros ++;
-            }*/
-
-        }
-        printf("LOOK THE HANGMAN: %s \n", hangmanstring);
-        
-        
-        if(erros == 1){
-
-        printf("YOU HAVE %d \n", erros);
+        printf("YOU HAVE %d \n", (*erros));
         printf("\n  _______            \n");
         printf(" |/      |          \n");
         printf(" |                  \n");
@@ -69,8 +64,8 @@ int main(void){
         printf("_|___               \n");
 
         } 
-        else if(erros == 2){
-             printf("YOU HAVE %d \n", erros);
+        else if((*erros) == 2){
+             printf("YOU HAVE %d \n", (*erros));
             printf("\n  _______           \n");
             printf(" |/      |          \n");
             printf(" |      (_)         \n");
@@ -80,8 +75,8 @@ int main(void){
             printf(" |                  \n");
             printf("_|___               \n");
         } 
-        else if(erros == 3){
-             printf("YOU HAVE %d\n", erros);
+        else if((*erros) == 3){
+             printf("YOU HAVE %d\n", (*erros));
             printf("\n  _______            \n"); 
             printf(" |/      |           \n");
             printf(" |      (_)          \n");
@@ -92,9 +87,9 @@ int main(void){
             printf("_|___                \n");
 
         } 
-        else if (erros == 4){
+        else if ((*erros) == 4){
 
-            printf("YOU HAVE %d\n", erros);
+            printf("YOU HAVE %d\n", (*erros));
             printf("\n  _______            \n"); 
             printf(" |/      |           \n");
             printf(" |      (_)          \n");
@@ -104,8 +99,8 @@ int main(void){
             printf(" |                   \n");  
             printf("_|___                \n");
         }
-        else if(erros == 5){
-            printf("YOU HAVE %d\n", erros);
+        else if((*erros) == 5){
+            printf("YOU HAVE %d\n", (*erros));
             printf("\n  _______            \n"); 
             printf(" |/      |           \n");
             printf(" |      (_)          \n");
@@ -115,56 +110,75 @@ int main(void){
             printf(" |                   \n");  
             printf("_|___                \n");
 
-            enforcou = 1;
-        }
-        else if (hits <= strlen(secretyword)-1){
-            acertou = 1;
-        }
-        
-    }
+            (*enforcou) = 1;
 
+            youwrong();
+        }
+        else if ((*hits) >= manipulador){
+            (*acertou) = 1;
+            youwin();
+        }
+
+
+}
+        
+
+
+int main(void){
+
+    opengame();
+
+    // palavras são arrays que deve ser colocados o qtd de caracteres dentro de []
+    // sprintf printa todo o array não somente palavra por palavra
+    char secretyword[20];
+    sprintf(secretyword, "watermelon");
+
+    int acertou, enforcou, erros, hits;
+    acertou = 0;
+    enforcou = 0;
+    erros = 0;
+    hits = 0;
+    
+    
+    char hangmanstring[strlen(secretyword)];
 
     
-printf("     OH MY GODS YOU BE HANGED   \n");
-printf("                                \n");
-printf("    _______________             \n");  
-printf("   /               \\           \n");
-printf("  /                 \\          \n");
-printf("//                   \\/\\      \n");
-printf("\\|   XXXX     XXXX   | /       \n");
-printf(" |   XXXX     XXXX   |/         \n");
-printf(" |   XXX       XXX   |          \n");
-printf(" |                   |          \n");
-printf(" \\__      XXX      __/         \n");
-printf("   |\\     XXX     /|           \n");
-printf("   | |           | |            \n");
-printf("   | I I I I I I I |            \n");
-printf("   |  I I I I I I  |            \n");
-printf("   \\_             _/           \n");
-printf("     \\_         _/             \n");
-printf("       \\_______/               \n");
+    for( int y = 0; y < strlen(secretyword); y ++){
+        hangmanstring[y] = '_';
+    }
 
+    while(acertou == 0 && enforcou == 0){
 
-printf(" CONGRATULATIONS YOU WIN \n");
-printf("                         \n");
-printf("       ___________       \n");
-printf("      '._==_==_=_.'      \n");
-printf("      .-\\:      /-.      \n");
-printf("     | (|:.     |) |     \n");
-printf("      '-|:.     |-'      \n");
-printf("        \\::.    /        \n");
-printf("         '::. .'         \n");
-printf("           ) (           \n");
-printf("         _.' '._         \n");
-printf("        '-------'        \n");
+        char letter;
+        printf("enter your first guess for the letter in the secret word. \n");
+        printf("rights: %d end erros: %d \n", hits, erros);
+        scanf(" %c", &letter);
 
+        int achou = 0;
+        for(int c = 0; c <= strlen(secretyword); c++){
+            if(secretyword[c] == letter){
+                printf("you hit it!!! \n");
+                hangmanstring[c] = letter;
+                hits ++;
+                achou ++;
 
+            }
+        }
 
+        
+        if(achou == 0){
+            erros += 1;
 
-              
-    printf("\n-----------------------------------------------------------\n");
-    printf("----------------------THE END GAME-------------------------\n");
-    printf("-----------------------------------------------------------\n");
+        }
+
+        printf("LOOK THE HANGMAN: %s \n", hangmanstring);
+
+        verificar(&erros, &hits, strlen(secretyword), &enforcou, &acertou);
+        
+        
+    }
+     
+    finalgame();
 
     return 0;
 }
